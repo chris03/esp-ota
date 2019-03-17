773cec433ec70757c674c06eb09f9ebc25d9ff4d
chris03 <gitkraken@chris03.com>
2019-03-16 22:47:05 -0400

    Remove ADC

--- src/web.h
+++ src/web.h
@@ -1,9 +1,6 @@
 #include <ESP8266WebServer.h>
 ESP8266WebServer server(80);
 
-// Enable built-in ADC
-ADC_MODE(ADC_VCC);
-
 const char *plainContentType = "text/plain";
 size_t len;
 char buff[512];
@@ -14,14 +11,13 @@ void setupWeb()
     server.on("/", []() {
         // Format output
         len = snprintf(buff, sizeof(buff),
-                       "Version: %s\nFlashChipRealSize: %i\nFlashChipId: %i\nFlashChipSizeByChipId: %i\nFreeSketchSpace: %i\nFreeHeap: %i\nVCC: %i\nCPU Freq: %i",
+                       "Version: %s\nFlashChipRealSize: %i\nFlashChipId: %i\nFlashChipSizeByChipId: %i\nFreeSketchSpace: %i\nFreeHeap: %i\nCPU Freq: %i",
                        ESP.getFullVersion().c_str(),
                        ESP.getFlashChipRealSize() / 1024,
                        ESP.getFlashChipId(),
                        ESP.getFlashChipSizeByChipId(),
                        ESP.getFreeSketchSpace(),
                        ESP.getFreeHeap(),
-                       ESP.getVcc(),
                        ESP.getCpuFreqMHz());
 
         server.send_P(200, plainContentType, buff, len);
